8ecc48d0f244b946e5662aa23625be4fe55ce0d1
danny <a68800886a@gmail.com>
2025-04-25 20:23:55 +0800

    4/25課間紀錄

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
 import pymysql
@@ -9,10 +9,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # return f"<h1>Hell World!</h1>\n{datetime.now()}"
+    books = []
+
+    if books:
+        for book in books:
+            print(book["name"])
+            print(book["price"])
+            print(book["image_url"])
+    else:
+        print("販售完畢，目前無書籍")
     username = "Danny"
     nowtime = datetime.now().strftime("%Y-%m-%d")
     print(username, nowtime)
-    return render_template("index.html", name=username, now=nowtime)
+    return render_template("index.html", name=username, now=nowtime, books=books)
 
 
 @app.route("/pm25-data")
@@ -22,6 +31,18 @@ def getpm25_data():
     df["datacreationdate"] = pd.to_datetime(df["datacreationdate"])
     df1 = df.dropna()
     return df1.values.tolist()
+
+
+@app.route("/bmi")
+def bmi():
+    # arge GET
+    height = eval(request.args.get("height"))
+    weight = eval(request.args.get("weight"))
+    print(height, weight)
+
+    bmi = round(weight / (height / 100) ** 2, 2)
+
+    return render_template("bmi.html", height=height, weight=weight, bmi=bmi)
 
 
 app.run(debug=True)
